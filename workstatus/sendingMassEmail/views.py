a1aefca1418b94c82b11a16b20460c61d603c6b9
Priscilla <priscilla@myplanetdigital.com>
2011-09-02 11:05:59 -0400

    modified compiled files and sendingMassEmail/views.py

--- workstatus/sendingMassEmail/views.py
+++ workstatus/sendingMassEmail/views.py
@@ -10,7 +10,7 @@ from django.core.mail import EmailMultiAlternatives #
 from django.template.loader import render_to_string #
 from django.utils.html import strip_tags #
 from workstatus.mail.models import *
-import feedparser
+from mail import feedparser
 import time
 
 
